669d7859e6d16b7e54e155bd49625db79cb7531f
Lordracfures <raul.pineda.caballero@gmail.com>
2022-10-18 12:25:55 +0200

    change od screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:receipapp/recipe_detail.dart';
 import 'recipe.dart';
 
 
 void main() {
-  runApp(const MyApp());
+  runApp(const RecipeApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class RecipeApp extends StatelessWidget {
+  const RecipeApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,8 +19,8 @@ class MyApp extends StatelessWidget {
       title: 'Recipe Calculator',
       theme: theme.copyWith(
         colorScheme: theme.colorScheme.copyWith(
-          primary: Colors.grey,
-          secondary: Colors.black,
+          primary: Color.fromARGB(255, 61, 31, 168),
+          secondary: Color.fromARGB(255, 156, 40, 40),
         ),
       ),
       home: const MyHomePage(title: 'Recipe Calculator'),
@@ -37,23 +38,76 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+ 
   @override
   Widget build(BuildContext context) {
+    //1
     return Scaffold(
+      //2
       appBar: AppBar(
         title: Text(widget.title),
+        centerTitle: true,
       ),
+      //3
       body: SafeArea(
-        child: Container()
+        // TODO: Replace child: Container()
+        // 4
+        child: ListView.builder(
+          itemCount: Recipe.samples.length,
+          itemBuilder: (BuildContext context, index) {
+
+            return GestureDetector(
+              onTap: (){
+                Navigator.push(
+                  context,
+                   MaterialPageRoute(builder: (context) {
+                     return RecipeDetail(recipe: Recipe.samples[index]); 
+                      }
+                    )
+                  );
+                },
+                child:  buildRecipeCard(Recipe.samples[index]),
+              );   
+            },
+          
+          ),
         ),
+      );
+  }
+  // TODO: Add buildRecipeCard() here
+  
+  Widget buildRecipeCard(Recipe recipe) {
+  // 1
+    return Card(
+      elevation: 5.0,
+    // 2
+      shape: RoundedRectangleBorder(
+       borderRadius: BorderRadius.circular(10.0)),
+     
+     child: Padding(
+      padding: const EdgeInsets.all(16.0),
+       child: Column(
+        // 
+        children: <Widget>[
+          // 4
+          Image(image: AssetImage(recipe.imageUrl)),
+          // 5
+          const SizedBox(
+              height: 14.0,
+          ),
+          Text(
+            recipe.label,
+            style: const TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.w700,
+              fontFamily: 'Palatino'     
+            )
+          ),
+        ],
+      ),
+     )
     );
   }
+
+  
 }
---
+++ lib/recipe_detail.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'recipe.dart';
+
+class RecipeDetail extends StatefulWidget {
+  final Recipe recipe;
+
+  const RecipeDetail({Key? key, required this.recipe,}) : super(key: key);
+
+  @override
+  State<RecipeDetail> createState() {
+    return _RecipeDetailState();
+  }
+
+  /*
+     @override
+    _RecipeDetailState createState() {
+    return _RecipeDetailState();
+    }
+    */
+
+}
+
+class _RecipeDetailState extends State<RecipeDetail> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.recipe.label)
+        ),
+      body:  SafeArea(
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 300,
+                    width: double.infinity,
+                    child: Image(
+                      image: AssetImage(widget.recipe.imageUrl) 
+                      )
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      widget.recipe.label,
+                      style: const TextStyle(fontSize: 18),
+                    )
+                    //slider
+                 ]
+                ),
+              ),
+            );
+          }
+  }
